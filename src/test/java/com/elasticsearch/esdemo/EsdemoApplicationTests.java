abde2156e13fd5708c1d838fee6371e5a7e8e6c6
ramit21 <21.ramit@gmail.com>
2020-07-18 15:22:42 +0530

    es

---
+++ src/test/java/com/elasticsearch/esdemo/EsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.esdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
